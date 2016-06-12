93441948a16b3a53facc9b1390683f66d3fa8399
Landon Bass <landon@labtechnologygroup.com>
2016-06-12 08:38:17 -0400

    more active patterns

--- fsxSrc/activePatterns.fsx
+++ fsxSrc/activePatterns.fsx
@@ -10,3 +10,22 @@ let testNumber input =
 testNumber 3
 testNumber 100
 
+let (|Int|_|) str =
+    match System.Int32.TryParse(str) with 
+    | (true, int) -> Some(int)
+    | _ -> None
+
+let (|Bool|_|) str =
+    match System.Boolean.TryParse(str) with 
+    | (true, bool) -> Some(bool)
+    | _ -> None
+
+let testParse str =
+    match str with 
+    | Int i  -> printfn "int: %i" i
+    | Bool b -> printfn "bool: %b" b 
+    | _      -> printfn "something else: %s" str
+
+testParse "12"
+testParse "false"
+testParse "abc"
